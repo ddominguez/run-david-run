8fbeb4927b9d72d198afe42386a784d7bccc366e
David Dominguez <352985+ddominguez@users.noreply.github.com>
2023-06-20 22:10:33 -0400

    added db package

--- cmd/syncdb/main.go
+++ cmd/syncdb/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ddominguez/run-david-run/db"
+)
 
 func main() {
-    fmt.Println("sync command that will populate db with race data.")
+	// get access token from db
+	// -- if access token exists check if expired
+	// -- if expired use refresh token to new access token and save to db
+
+	// if access token does not exist get new access token data and save to db
+
+	// use valid access token to fetch race activities from stava and save to db
+
+	// save athlete data if not exists
+
+	dbUrl := fmt.Sprintf("postgres://%s:%s@localhost:5432/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	pgxDB, err := db.NewPgxConn(dbUrl)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer pgxDB.Pool.Close()
+
+	sa, err := db.SelectStravaAuth(pgxDB)
+	if err != nil {
+		log.Println("strava auth data not found.", err)
+	}
+	if !sa.Exists() {
+		log.Println("get new strava auth data")
+	}
+
 }
---
+++ db/db.go
@@ -0,0 +1,54 @@
+package db
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type pgxConn struct {
+	Pool *pgxpool.Pool
+}
+
+// NewPgxConn creates and return a new pool connection
+func NewPgxConn(dbUrl string) (*pgxConn, error) {
+	pool, err := pgxpool.New(context.Background(), dbUrl)
+	if err != nil {
+		return nil, err
+	}
+	return &pgxConn{Pool: pool}, nil
+}
+
+type stravaAuth struct {
+	AccessToken  string
+	ExpiresAt    uint64
+	RefreshToken string
+	AthleteId    uint64
+}
+
+func (s *stravaAuth) Exists() bool {
+	return s.AthleteId > 0
+}
+
+// SelectStravaAuth selects a single strava_auth record
+func SelectStravaAuth(pgxConn *pgxConn) (stravaAuth, error) {
+	q := `SELECT access_token, access_token_expires_at, refresh_token, athlete_id FROM strava_auth LIMIT 1`
+	var res stravaAuth
+	err := pgxConn.Pool.
+		QueryRow(context.Background(), q).
+		Scan(&res.AccessToken, &res.ExpiresAt, &res.RefreshToken, &res.AthleteId)
+	if err != nil {
+		return res, err
+	}
+	return res, nil
+}
+
+// InsertStravaAuth inserts a new strava_auth record
+func InsertStravaAuth(pgxConn *pgxConn, accessToken, refreshToken string, expiresAt, athleteId uint64) error {
+	q := `INSERT INTO strava_auth(access_token, access_token_expires_at, refresh_token, athlete_id) VALUES ($1, $2, $3, $4)`
+	_, err := pgxConn.Pool.Exec(context.Background(), q, accessToken, expiresAt, refreshToken, athleteId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
