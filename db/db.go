b9c45d40b588413fc624f46296d4ebece2bebde6
David Dominguez <352985+ddominguez@users.noreply.github.com>
2023-06-26 22:11:15 -0400

    updated exports

--- db/db.go
+++ db/db.go
@@ -6,35 +6,35 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type pgxConn struct {
+type PgxConn struct {
 	Pool *pgxpool.Pool
 }
 
 // NewPgxConn creates and returns a new pool connection
-func NewPgxConn(dbUrl string) (*pgxConn, error) {
+func NewPgxConn(dbUrl string) (*PgxConn, error) {
 	pool, err := pgxpool.New(context.Background(), dbUrl)
 	if err != nil {
 		return nil, err
 	}
-	return &pgxConn{Pool: pool}, nil
+	return &PgxConn{Pool: pool}, nil
 }
 
-//stravaAuth represents db table strava_auth
-type stravaAuth struct {
+// StravaAuth represents db table strava_auth
+type StravaAuth struct {
 	AccessToken  string
 	ExpiresAt    uint64
 	RefreshToken string
 	AthleteId    uint64
 }
 
-func (s *stravaAuth) Exists() bool {
+func (s *StravaAuth) Exists() bool {
 	return s.AthleteId > 0
 }
 
 // SelectStravaAuth selects and returns a single strava_auth record
-func SelectStravaAuth(pgxConn *pgxConn) (stravaAuth, error) {
+func SelectStravaAuth(pgxConn *PgxConn) (StravaAuth, error) {
 	q := `SELECT access_token, access_token_expires_at, refresh_token, athlete_id FROM strava_auth LIMIT 1`
-	var res stravaAuth
+	var res StravaAuth
 	err := pgxConn.Pool.
 		QueryRow(context.Background(), q).
 		Scan(&res.AccessToken, &res.ExpiresAt, &res.RefreshToken, &res.AthleteId)
@@ -45,7 +45,7 @@ func SelectStravaAuth(pgxConn *pgxConn) (stravaAuth, error) {
 }
 
 // InsertStravaAuth inserts a new strava_auth record
-func InsertStravaAuth(pgxConn *pgxConn, accessToken, refreshToken string, expiresAt, athleteId uint64) error {
+func InsertStravaAuth(pgxConn *PgxConn, accessToken, refreshToken string, expiresAt, athleteId uint64) error {
 	q := `INSERT INTO strava_auth(access_token, access_token_expires_at, refresh_token, athlete_id) VALUES ($1, $2, $3, $4)`
 	_, err := pgxConn.Pool.Exec(context.Background(), q, accessToken, expiresAt, refreshToken, athleteId)
 	if err != nil {
